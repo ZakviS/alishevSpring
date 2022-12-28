f09cdbf6492968445dd72973488a760df006d416
ZakviS <lesha.grinuk@gmail.com>
2022-12-28 18:36:06 +0300

    6.4 lesson

--- src/main/java/by/grinuk/springcourse/MusicPlayer.java
+++ src/main/java/by/grinuk/springcourse/MusicPlayer.java
@@ -1,7 +1,14 @@
 package by.grinuk.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    private List<Music> musicList = new ArrayList<>();
 
     private String name;
     private int volume;
@@ -22,17 +29,13 @@ public class MusicPlayer {
         this.volume = volume;
     }
 
-    public MusicPlayer(Music music){
-        this.music=music;
-    }
-
 
     public MusicPlayer(){};
-    public void setMusic(Music music) {
-        this.music = music;
-    }
+
 
     public void playMusic(){
-        System.out.println("Playing " + music.getSong());
+        for(Music music : musicList){
+            System.out.println("Playing " + music.getSong());
+        }
     }
 }
---
+++ src/main/java/by/grinuk/springcourse/RapMusic.java
@@ -0,0 +1,8 @@
+package by.grinuk.springcourse;
+
+public class RapMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Rap Song";
+    }
+}
