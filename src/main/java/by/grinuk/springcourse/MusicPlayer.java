bfa43ea4968415d3d9303f25fc22011dcd837244
ZakviS <lesha.grinuk@gmail.com>
2023-02-14 13:58:51 +0300

    11 lesson

--- src/main/java/by/grinuk/springcourse/MusicPlayer.java
+++ src/main/java/by/grinuk/springcourse/MusicPlayer.java
@@ -1,18 +1,23 @@
 package by.grinuk.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
+
+
+    private Music music1;
+    private Music music2;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic) {
-        this.classicalMusic = classicalMusic;
+    public MusicPlayer(@Qualifier("classicalMusic") Music music1, @Qualifier("musicBean") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public void playMusic() {
-        System.out.println("Playing: " + classicalMusic.getSong());
+        System.out.println("Playing: " + music1.getSong() + ", " + music2.getSong());
     }
 }
